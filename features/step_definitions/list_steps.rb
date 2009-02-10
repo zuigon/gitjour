8a9cf09a8415b6886dcdcbe58732a07b72ca753d
DylanFM <dylan.fm@gmail.com>
2009-02-10 21:30:15 +0800

    - Gitjour list output's data is pulled into hash for easier testing - Number of services in list's output is checked against number returned by service_list method
    Signed-off-by: Lachlan Hardy <lachlan@lachstock.com.au>

--- features/step_definitions/list_steps.rb
+++ features/step_definitions/list_steps.rb
@@ -18,8 +18,36 @@ end
 When /^I run gitjour list$/ do
   When "I capture stdout"
   Gitjour::Application.run "list"
-  output = @new_stdout.string
+  @output = @new_stdout.string
   When "I put stdout back"
+  # Build some regular expressions
+  # "Gathering for up to 5 seconds..."
+  intro_line = /^[\w\s]+(\d+\s\w+)\.{3}/
+  # "=== surfcomp"
+  repo_pattern = /^={3}\s([\w-]+)/
+  # "\tdylanfm-surfcomp git://tubed.local/" (with optional port)
+  service_pattern = /^\s+([\w\-]+)\s(git:\/\/[\w\d\.-]+(\/|\:\d+\/))/ 
+  # "2 repositories shown."
+  number_shown_pattern = /^(\d+)\s\w+\s\w+\./
+  # Build a hash like @repositories so we can test list's output easier
+  @list_repositories = Hash.new
+  # Let's make sure there are the same amount of services in the output as there are in @repositories
+  num_services = 0
+  @repositories.each_value { |services| num_services += services.size }
+  list_num_services = 0
+  # Now let's process the list output
+  @output.split('\n')
+  @output.each do |line|
+    if repo_pattern =~ line
+      repo = Regexp.last_match(0).to_sym
+      @current_repo = repo
+      @list_repositories[repo] = [] unless @list_repositories.has_key? repo
+    elsif service_pattern =~ line
+      list_num_services += 1
+      @list_repositories[@current_repo] << { :name => Regexp.last_match(0), :url => Regexp.last_match(1) }
+    end
+  end
+  list_num_services.should == num_services
 end
 
 Then /^for each repository I should see its name$/ do
